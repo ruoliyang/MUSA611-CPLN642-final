61ac53bc9bca1105b0ef025c47422f0e7f980377
ruoliyang <60238166+ruoliyang@users.noreply.github.com>
2020-05-09 21:21:26 -0400

    update

--- main.js
+++ main.js
@@ -16,15 +16,17 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
 var dataset = "https://raw.githubusercontent.com/ruoliyang/MUSA611-CPLN642-midterm/master/PPR_Assets.geojson";
 var featureGroup;
 
-//slide 1
+//front page
 var myFilter = function(feature) {
   return feature.properties.PPR_USE !== ' '
 };
 
+/*
 //slide 2
 var myFilter2 = function(feature2) {
   return feature2.properties.ACREAGE >= 50;
 };
+*/
 
 /*
 var myStyle2 = function (feature2) {
@@ -37,11 +39,12 @@ var myStyle2 = function (feature2) {
 };
 */
 
+/*
 //slide 3
 var myFilter3 = function(feature3) {
   return feature3.properties.ACREAGE <= 30;
 };
-
+*/
 /*
 var myStyle3 = function (feature3) {
   switch (feature.properties.ACREAGE) {
@@ -50,28 +53,47 @@ var myStyle3 = function (feature3) {
 };
 */
 
-//slide 4
+//Resident Page
 var myFilter4 = function(feature) {
   return (feature.properties.PPR_USE == 'RECREATION_SITE' ||
          feature.properties.PPR_USE == 'NEIGHBORHOOD_PARK' ||
          feature.properties.PPR_USE == 'COMMUNITY_PARK' ||
-         feature.properties.PPR_USE == 'MINI_PARK');
+         feature.properties.PPR_USE == 'MINI_PARK' ||
+         feature.properties.PPR_USE == 'POOL' ||
+         feature.properties.PPR_USE == 'ICE_RINK' ||
+         feature.properties.PPR_USE == 'GOLF' ||
+         feature.properties.PPR_USE == 'ATHLETIC');
 };
 
 var myStyle = function(feature) {
   switch (feature.properties.PPR_USE) {
-    case 'RECREATION_SITE': return {color: '#660000'};
-    case 'NEIGHBORHOOD_PARK': return {color: '#994C00'};
-    case 'COMMUNITY_PARK': return {color: '#FF8000'};
-    case 'MINI_PARK': return {color: '#00994C'};
-    //case 'FRI': return {color: '#004C99'};
-    //case 'SAT': return {color: '#CC0066'};
-    //case 'SUN': return {color: '#0080FF'};
+    case 'RECREATION_SITE': return {color: '#FF9AA2'};
+    case 'NEIGHBORHOOD_PARK': return {color: '#FFB7B2'};
+    case 'COMMUNITY_PARK': return {color: '#FFDAC10'};
+    case 'MINI_PARK': return {color: '#E2F0CB'};
+    case 'POOL': return {color: '#B5EAD7'};
+    case 'ICE_RINK': return {color: '#C7CEEA'};
+    case 'GOLF': return {color: '#C2C2B4'};
+    case 'ATHLETIC': return {color: '#98B1B6'};
 //      break;
 //      default:
 //  return {};
   }
 };
+
+//marker popup
+featureGroup = L.geoJson(parsedData, {
+      style: myStyle,
+      //filter: myFilter
+      pointToLayer: function (feature, latlng) {
+         return L.marker(latlng, {icon: smallIcon});
+      //  return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+      onEachFeature: function (feature, layer) {
+ layer.bindPopup('Name'+feature.properties.ASSET_NAME+'Address'+feature.properties.ADDRESS);
+}
+    }).addTo(map);
+
 
 //slide 5 ZOOM!
 var myFilter5 = function(feature) {
@@ -89,6 +111,33 @@ var myStyle2 = function(feature) {
     case 19106: return {color: '#0080FF'};
     case 19107: return {color: '#00FF00'};
   }};
+
+//Visitors Page
+  var myFilter6 = function(feature) {
+    return (feature.properties.PPR_USE == 'GREENWAY_PARKWAY' ||
+           feature.properties.PPR_USE == 'REGIONAL_CONSERVATION_WATERSHED' ||
+           feature.properties.PPR_USE == 'ENVIRONMENTAL_EDUCATION_CENTER' ||
+           feature.properties.PPR_USE == 'MUSEUM' ||
+           feature.properties.PPR_USE == 'GARDEN' ||
+           feature.properties.PPR_USE == 'ZOO_HABITAT' ||
+           feature.properties.PPR_USE == 'RESERVOIR');
+  };
+
+  var myStyle = function(feature) {
+    switch (feature.properties.PPR_USE) {
+      case 'GREENWAY_PARKWAY': return {color: '#5EBD3E'};
+      case 'REGIONAL_CONSERVATION_WATERSHED': return {color: '#FFB900'};
+      case 'ENVIRONMENTAL_EDUCATION_CENTER': return {color: '#F78200'};
+      case 'MUSEUM': return {color: '#E23838'};
+      case 'GARDEN': return {color: '#973999'};
+      case 'ZOO_HABITAT': return {color: '#009CDF'};
+      case 'RESERVOIR': return {color: '#EE82EE'};
+  //      break;
+  //      default:
+  //  return {};
+    }
+  };
+
 
 var showResults = function() {
   /* =====================
